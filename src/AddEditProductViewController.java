7a6e9f4ec04ebb248407e4ddaf01498966407a61
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2014-11-28 14:23:18 +0000

    Created AddEditProductViewController. Also wrote some code in AppManager that creates and presents an AddEditProductViewController

---
+++ src/AddEditProductViewController.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by kylejm on 28/11/14.
+ */
+public class AddEditProductViewController extends JFrame {
+
+    private JTextField textField1;
+    private JPanel panel1;
+
+    public AddEditProductViewController() {
+        this.add(textField1);
+    }
+}
--- src/ApplicationManager.java
+++ src/ApplicationManager.java
@@ -1,8 +1,14 @@
+import javax.swing.*;
+
 /**
  * Created by kylejm on 28/11/14.
  */
 public class ApplicationManager {
     public static void main(String[] args) {
-
+        AddEditProductViewController vc = new AddEditProductViewController();
+        vc.setSize(300, 200);
+        vc.setTitle("Add Product");
+        vc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        vc.setVisible(true);
     }
 }
