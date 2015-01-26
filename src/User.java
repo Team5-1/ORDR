103a338ae60d738bf4fe5c5f60b05289057ead0f
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-01-26 12:57:40 +0000

    Added some fields to User

--- src/User.java
+++ src/User.java
@@ -1,10 +1,36 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  * Created by kylejm on 25/01/15.
  */
-public class User {
+public class User extends ORDRSQLObject {
+    //Fetched DB fields
+    private String fetchedUsername;
+    private String fetchedFirstName;
+    private String fetchedLastName;
+    private String fetchedEmailAddress;
 
-    public User() {
-        //TODO: pass in SQL data and init
+    //Current DB fields
+    private int ID;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+    //Constructors
+    public User(String username, String firstName, String lastName) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    private User() {}
+
+    @Override
+    public Boolean hasChanges() {
+        return (username != null || firstName != null || lastName != null);
     }
 
 }
