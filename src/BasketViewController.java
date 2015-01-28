f9ecf3799522807a2ecb1b5afceb7392dc448887
Ali Younas <ali.younas@my.westminster.ac.uk>
2015-01-28 11:55:29 +0000

    update
    update

--- src/BasketViewController.java
+++ src/BasketViewController.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 /**
  * Created by aliyounas on 26/01/15.
  */
 public class BasketViewController {
+
+
+    private JTextField quantityTextField;
+    private JScrollPane ProductList;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
