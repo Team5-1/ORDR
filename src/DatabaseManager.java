cfbf3f6eaf5110e5734b018f50a9165baf52dd1e
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-02-09 17:09:24 +0000

    Added selectFields... and queryTable... methods

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -41,6 +41,65 @@ public class DatabaseManager {
         });
     }
 
+    public static void fetchFieldsForTableInBackground(final ArrayList<String> fields, final String tableName, final SQLQueryCompletionHandler handler) {
+        BackgroundQueue.addToQueue(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String stmString = "SELECT ";
+                    int fieldCount = fields.size();
+                    int i = 0;
+                    for (String field : fields) {
+                        stmString += (i == fieldCount) ? field + " " : field + ", ";
+                        i++;
+                    }
+                    stmString += "FROM " + tableName;
+                    PreparedStatement stm = sharedManager.dbConnection.prepareStatement(stmString);
+                    ResultSet results = stm.executeQuery();
+                    handler.succeeded(results);
+                } catch (SQLException e) {
+                    handler.failed(e);
+                }
+            }
+        });
+    }
+
+    public static void queryTableInBackground(final HashMap<String, Object> fieldsAndValuesToMatch, final ArrayList<String> fieldsToFetch, final String tableName, final SQLQueryCompletionHandler handler) {
+        BackgroundQueue.addToQueue(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String stmString = "SELECT ";
+                    int fieldCount = fieldsToFetch.size();
+                    int i = 0;
+                    for (String field : fieldsToFetch) {
+                        stmString += (i == fieldCount) ? field + ", " : field + " ";
+                        i++;
+                    }
+                    stmString += "FROM " + tableName + " WHERE ";
+                    ArrayList<Object> values = new ArrayList<Object>();
+                    fieldCount = fieldsAndValuesToMatch.size();
+                    i = 1;
+                    for (String key : fieldsAndValuesToMatch.keySet()) {
+                        stmString += key + " = ?";
+                        if (i != fieldCount) stmString +=  ", ";
+                        values.add(fieldsAndValuesToMatch.get(key));
+                        i++;
+                    }
+                    PreparedStatement stm = sharedManager.dbConnection.prepareStatement(stmString);
+                    for (i = 0; i < values.size(); i++) {
+                        stm.setObject(i + 1, values.get(i));
+                    }
+                    ResultSet result = stm.executeQuery();
+                    handler.succeeded(result);
+                } catch (SQLException e) {
+                    handler.failed(e);
+                }
+            }
+        });
+    }
+
+
     public static void updateFieldsForRecord(final String tableName, final String recordKeyName, final int recordKey, final HashMap<String, Object> columnsAndValues, final SQLSaveCompletionHandler handler) {
         BackgroundQueue.addToQueue(new Runnable() {
             @Override
