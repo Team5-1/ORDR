00c980663d84eb487d39bd8d27bf0d18338d45a1
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-02-09 17:11:30 +0000

    WIP: Initial implementation of user sign-up

--- src/User.java
+++ src/User.java
@@ -1,4 +1,8 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by kylejm on 25/01/15.
@@ -20,10 +24,10 @@ public class User extends SQLObject {
     private String emailAddress;
 
     //Constructors
-    public User(String username, String firstName, String lastName) {
-        this.username = username;
+    public User(String firstName, String lastName, String emailAddress) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.emailAddress = emailAddress;
     }
 
     private User() {}
@@ -38,6 +42,38 @@ public class User extends SQLObject {
         return null;
     }
 
+    public void signUpUser(String password, final UserSignUpCompletionHandler handler) {
+        //Validation
+        if ID != null {
+            handler.succeeded(this);
+            return;
+        }
+        Pattern emailRegex = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = emailRegex.matcher(emailAddress);
+        if (!matcher.find()) {
+            handler.emailFormatIncorrect();
+            return;
+        }
+        if (password.length() < 8) {
+            handler.passwordTooShort();
+            return;
+        }
+
+        String stmString = "INSERT INTO " + getSQLTableName(User.class) + " SET first_name = '" + firstName + "', last_name = '" + lastName + "', email = '" + emailAddress + "', password = MD5('" + password + "')";
+        try {
+            PreparedStatement stm = DatabaseManager.sharedManager.getDbConnection().prepareStatement(stmString);
+            stm.execute();
+            handler.succeeded();
+        } catch (SQLException e) {
+            if (e.getErrorCode() == 1062) {
+                handler.emailAddressTaken();
+            } else {
+                handler.failed(e);
+            }
+        }
+    }
+
+    //Accessor methods
     @Override
     public Boolean hasChanges() {
         return (username != null || firstName != null || lastName != null);
@@ -51,5 +87,13 @@ public class User extends SQLObject {
     @Override
     public int getID() {
         return ID;
+    }
+
+    public interface UserSignUpCompletionHandler {
+        public void succeeded();
+        public void failed(SQLException exception);
+        public void passwordTooShort();
+        public void emailFormatIncorrect();
+        public void emailAddressTaken();
     }
 }
