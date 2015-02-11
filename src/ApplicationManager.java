abeb4a2d4f8fe6a2b54898a359cfda6e17cc29ba
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-02-11 12:14:40 +0000

    Created LogInViewController

--- src/ApplicationManager.java
+++ src/ApplicationManager.java
@@ -1,60 +1,15 @@
 import javax.swing.*;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  * Created by kylejm on 28/11/14.
  */
 public class ApplicationManager {
     public static void main(String[] args) {
-//        AddEditProductViewController vc = new AddEditProductViewController();
-//        vc.setSize(300, 200);
-//        vc.setTitle("Add Product");
-//        vc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        vc.setVisible(true);
-        Item.fetchAllItemsInBackground(new Item.MultipleItemCompletionHandler() {
-            @Override
-            public void succeeded(ArrayList<Item> items) {
-                for (Item item : items) {
-                    System.out.printf("%d\n", item.getID());
-                    System.out.println(item.getName());
-                    System.out.println(item.getDescription());
-                }
-                final Scanner scanner = new Scanner(System.in);
-                String newName = scanner.nextLine();
-                final Item item = items.get(0);
-                item.setName(newName);
-                item.save(new DatabaseManager.SQLSaveCompletionHandler() {
-                    @Override
-                    public void succeeded() {
-                        System.out.println("YAY!");
-                        String anotherName = scanner.nextLine();
-                        item.setName(anotherName);
-                        item.save(new DatabaseManager.SQLSaveCompletionHandler() {
-                            @Override
-                            public void succeeded() {
-                                System.out.println("YAY!");
-                            }
-
-                            @Override
-                            public void failed(SQLException exception) {
-                                System.out.println("BOO!");
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void failed(SQLException exception) {
-                        System.out.println("BOO!");
-                    }
-                });
-            }
-
-            @Override
-            public void failed(SQLException exception) {
-
-            }
-        });
+        JFrame window = new JFrame("ORDR");
+        window.setSize(300, 200);
+        LogInViewController vc = new LogInViewController();
+        window.add(vc.getView());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
     }
 }
