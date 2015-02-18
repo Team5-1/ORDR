82c04adfb861115fd436667e05ef80e3e7571f8e
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-02-18 22:27:18 +0000

    Started implementing a nav bar for the whole app

--- src/ApplicationManager.java
+++ src/ApplicationManager.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by kylejm on 28/11/14.
@@ -7,9 +8,14 @@ public class ApplicationManager {
 
     public static void applicationDidLaunch() {
         JFrame window = new JFrame("ORDR");
+        JPanel mainView = new JPanel(new BorderLayout());
         window.setSize(300, 200);
-        LogInViewController vc = new LogInViewController();
-        window.add(vc.getView());
+        window.add(mainView);
+        mainView.add(new LogInViewController().getView(), BorderLayout.CENTER);
+        JPanel nav = new JPanel();
+        nav.add(new JButton("Log in"), BorderLayout.NORTH);
+        nav.add(new JButton("Create account"), BorderLayout.NORTH);
+        mainView.add(nav, BorderLayout.NORTH);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
     }
