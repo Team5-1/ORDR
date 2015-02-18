12fea1cc9329033e6083570580c22ab2a8b03a62
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-02-18 23:35:53 +0000

    CreateAccountVC created. Navigate bar switches between createVC and logInVC

--- src/ApplicationManager.java
+++ src/ApplicationManager.java
@@ -1,20 +1,49 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by kylejm on 28/11/14.
  */
 public class ApplicationManager {
 
+    private static final LogInViewController logInVC = new LogInViewController();
+    private static final CreateAccountViewController createAccVC = new CreateAccountViewController();
+
     public static void applicationDidLaunch() {
         JFrame window = new JFrame("ORDR");
-        JPanel mainView = new JPanel(new BorderLayout());
+        final JPanel mainView = new JPanel(new BorderLayout());
         window.setSize(300, 200);
         window.add(mainView);
-        mainView.add(new LogInViewController().getView(), BorderLayout.CENTER);
+        mainView.add(logInVC.getView(), BorderLayout.CENTER);
         JPanel nav = new JPanel();
-        nav.add(new JButton("Log in"), BorderLayout.NORTH);
-        nav.add(new JButton("Create account"), BorderLayout.NORTH);
+        final JButton logInButton = new JButton("Log in");
+        logInButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (!logInVC.getView().isDisplayable()) {
+                    mainView.remove(createAccVC.getView());
+                    mainView.add(logInVC.getView(), BorderLayout.CENTER);
+                    mainView.revalidate();
+                    mainView.repaint();
+                }
+            }
+        });
+        final JButton createButton = new JButton("Create account");
+        createButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (!createAccVC.getView().isDisplayable()) {
+                    mainView.remove(logInVC.getView());
+                    mainView.add(createAccVC.getView(),  BorderLayout.CENTER);
+                    mainView.revalidate();
+                    mainView.repaint();
+                }
+            }
+        });
+        nav.add(logInButton, BorderLayout.NORTH);
+        nav.add(createButton, BorderLayout.NORTH);
         mainView.add(nav, BorderLayout.NORTH);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
---
+++ src/CreateAccountViewController.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by kylejm on 18/02/15.
+ */
+public class CreateAccountViewController {
+    private JTextField textField1;
+    private JPanel panel1;
+    private JTextField textField2;
+    private JTextField textField3;
+
+    public JPanel getView() {
+        return panel1;
+    }
+}
