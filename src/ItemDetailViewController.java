73fa046643dad1becd49b755963595e8d489fb51
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-03-13 16:12:23 +0000

    ItemDetailVC now implements ViewController

--- src/ItemDetailViewController.java
+++ src/ItemDetailViewController.java
@@ -6,7 +6,7 @@ import java.awt.event.ItemListener;
 /**
  * Created by MaywandA on 19/02/15.
  */
-public class ItemDetailViewController {
+public class ItemDetailViewController implements ViewController {
     private JLabel itemName;
     private JLabel price;
     private JLabel itemDescription;
